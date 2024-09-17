426e12f90abe1c433ed0730d3cf34499026b773e
kapil <kapil.personal9711@gmail.com>
2024-09-17 20:39:27 +0530

    new

--- frontend/src/services/uri.js
+++ frontend/src/services/uri.js
@@ -1,6 +1,4 @@
 // export default ;
-const protocol = location.protocol;
-let uri = "https://work-tracker-eight.vercel.app/api/v1";
-if (protocol === "http:") uri = "/api/v1";
+const uri = "/api/v1";
 
 export default uri;
